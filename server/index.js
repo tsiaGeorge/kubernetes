16bd61fed498c7b3594cb4ab4caf75a5b517380d
Georgios Tsiatzios <georgios.tsiatzios@kpn.com>
2020-01-31 15:45:07 +0100

    multiple pods for worker, 1 for the others

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ app.get('/values/current', async (req, res) => {
 app.post('/values', async (req, res) => {
   const index = req.body.index;
 
-  if (parseInt(index) > 140) {
+  if (parseInt(index) > 45) {
     return res.status(422).send('Index too high');
   }
 
